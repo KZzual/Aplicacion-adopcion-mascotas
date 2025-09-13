ff8a0a6efc10b0bf3d3e3e93a5a87357331ad90d
DIEGO_MEDEL <diegoaguil1126@gmail.com>
2025-09-13 16:58:56 -0300

    Secret Update 2

--- App/shopdog/shopdog-app/src/app/pages/home/home.page.ts
+++ App/shopdog/shopdog-app/src/app/pages/home/home.page.ts
@@ -10,4 +10,52 @@ import { IonicModule } from '@ionic/angular';
   styleUrls: ['./home.page.scss'],
   imports: [CommonModule, FormsModule, IonicModule]
 })
-export class HomePage {}
+export class HomePage {
+  pets = [
+    {
+      id: 1,
+      name: 'Cholito',
+      breed: 'Golden Retriever',
+      age: '2 años',
+      gender: 'Macho',
+      location: 'San Joaquín, Santiago de Chile',
+      vaccinated: true,
+      neutered: true,
+      houseTrained: true,
+      color: '#4A90E2'
+    },
+    {
+      id: 2,
+      name: 'Princesa',
+      breed: 'Siamese',
+      age: '1 años',
+      gender: 'Hembra',
+      location: 'San Joaquín, Santiago de Chile',
+      vaccinated: true,
+      neutered: true,
+      houseTrained: true,
+      color: '#FF6B6B'
+    }
+  ];
+
+  favorites: number[] = [];
+  selectedPet: any = null;
+
+  constructor() {}
+
+  openDetails(pet: any) {
+    this.selectedPet = pet;
+  }
+
+  closeDetails() {
+    this.selectedPet = null;
+  }
+
+  toggleFavorite(petId: number) {
+    if (this.favorites.includes(petId)) {
+      this.favorites = this.favorites.filter(id => id !== petId);
+    } else {
+      this.favorites.push(petId);
+    }
+  }
+}
