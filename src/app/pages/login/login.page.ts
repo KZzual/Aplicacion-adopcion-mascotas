c7beb33efe5ed049b452673d75a8f8807446f185
HernanMCHR <nomaslimites@gmail.com>
2025-10-04 23:14:58 -0300

    impl firebase

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { MenuController } from '@ionic/angular';
+import { IonicModule, MenuController, ToastController } from '@ionic/angular';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -12,10 +13,17 @@ import { MenuController } from '@ionic/angular';
   imports: [CommonModule, FormsModule, IonicModule]
 })
 export class LoginPage {
+  email: string = '';
+  password: string = '';
   passwordType: string = 'password';
   passwordIcon: string = 'eye-off';
 
-  constructor(private menuCtrl: MenuController) {}
+  constructor(
+    private menuCtrl: MenuController,
+    private authService: AuthService,
+    private toastCtrl: ToastController,
+     private router: Router
+  ) {}
 
   ionViewWillEnter() {
     this.menuCtrl.enable(false, 'main-menu'); // 🔒 bloquear menú
@@ -28,5 +36,38 @@ export class LoginPage {
   togglePassword() {
     this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
     this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+  }
+
+  async onLogin() {
+    try {
+      const user = await this.authService.login(this.email, this.password);
+      this.showToast('✅ Inicio de sesión correcto');
+      this.router.navigate(['/home']);   // ✅ aquí rediriges al Ho
+      console.log('Usuario:', user);
+      // Aquí podrías redirigir al home con router.navigate(['/home']);
+    } catch (error: any) {
+      this.showToast('❌ Error al iniciar sesión: ' + error.message);
+      console.error(error);
+    }
+  }
+
+  async onRegister() {
+    try {
+      const user = await this.authService.register(this.email, this.password);
+      this.showToast('✅ Registro exitoso');
+      console.log('Usuario registrado:', user);
+    } catch (error: any) {
+      this.showToast('❌ Error en registro: ' + error.message);
+      console.error(error);
+    }
+  }
+
+  private async showToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2500,
+      position: 'bottom'
+    });
+    await toast.present();
   }
 }
