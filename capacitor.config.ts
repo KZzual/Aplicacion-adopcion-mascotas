e4c780848a0ca09158b66e95606a32db64c71d25
Tomas-Duran-Leiva <tomas.leiva@aurafy.cl>
2025-10-13 11:18:04 -0300

    chore: update Angular and related dependencies to version 20.0.0
    - Upgraded Angular packages to version 20.0.0 for core, animations, common, compiler, forms, platform-browser, platform-browser-dynamic, router, and fire.
    - Updated devDependencies for Angular CLI, language service, and ESLint plugins to version 20.0.0.
    - Updated TypeScript to version 5.8.0.
    - Changed module resolution strategy in tsconfig.json from "node" to "bundler" and added "ignoreDeprecations" option.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'PetHub',
+  appName: 'pethub',
   webDir: 'www'
 };
 
