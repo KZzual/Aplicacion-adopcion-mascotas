dbc58d45dab96e7ba873177590e8c51e8decb879
Tomas-Duran-Leiva <tomas.leiva@aurafy.cl>
2025-10-18 16:31:12 -0300

    feat: actualizar configuración de Firebase en google-services.json y environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,22 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyD_W1tLMzIc2-iVPF8rePjqRFtYmL1DEug",
-    authDomain: "pethub-9975a.firebaseapp.com",
-    projectId: "pethub-9975a",
-    storageBucket: "pethub-9975a.appspot.com",
-    messagingSenderId: "714950945847",
-    appId: "1:714950945847:android:015cd5bc20e14f7863b2"
+    apiKey: "AIzaSyANQcZ-gRI6388Mms6LjYz9ByDzX4sftBU",
+    authDomain: "pethub-f8480.firebaseapp.com",
+    projectId: "pethub-f8480",
+    storageBucket: "pethub-f8480.firebasestorage.app",
+    messagingSenderId: "586451444437",
+    appId: "1:586451444437:web:234877b43c029e625e385c",
+    measurementId: "G-R2YYLLQDDE"
   }
+  //firebaseConfig: {
+  //  apiKey: "AIzaSyD_W1tLMzIc2-iVPF8rePjqRFtYmL1DEug",
+  //  authDomain: "pethub-9975a.firebaseapp.com",
+  //  projectId: "pethub-9975a",
+  //  storageBucket: "pethub-9975a.appspot.com",
+  //  messagingSenderId: "714950945847",
+  //  appId: "1:714950945847:android:015cd5bc20e14f7863b2"
+  //}
 };
 
 
