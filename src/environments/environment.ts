4e881a0061ebab664f8be422c259022123a40f70
HernanMCHR <nomaslimites@gmail.com>
2025-10-18 21:11:30 -0300

    se actualiza json firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyD_W1tLMzIc2-iVPF8rePjqRFtYmL1DEug",
-    authDomain: "pethub-9975a.firebaseapp.com",
-    projectId: "pethub-9975a",
-    storageBucket: "pethub-9975a.appspot.com",
-    messagingSenderId: "714950945847",
-    appId: "1:714950945847:android:015cd5bc20e14f7863b2"
+    apiKey: "AIzaSyDGYHntJlvrxJ18EVTzdzzwReG95Dn8hDM",
+    authDomain: "pethub-47125.firebaseapp.com",
+    projectId: "pethub-47125",
+    storageBucket: "pethub-47125.appspot.com",
+    messagingSenderId: "191915819129",
+    appId: "1:191915819129:android:7ebc5ab2086d2b386f54a8"
   }
 };
 
